5e81377371ee4e37a7a302cb253d3f050ad84d6a
binodbabu-paudel <167612041+binodbabu-paudel@users.noreply.github.com>
2025-12-01 19:54:57 +0545

    Initialize PDF Adjective Extractor package

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+PDF Adjective Extractor Package
+"""
+
+from .pdf_extractor import extract_text_from_pdf, clean_text
+from .text_analyzer import TextAnalyzer
+
+__version__ = "1.0.0"
+__author__ = "Binod Babu Paudel"
